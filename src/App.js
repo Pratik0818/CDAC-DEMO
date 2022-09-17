cc043d68907ac316f0350cdcb05fd27c2785e18d
pratik jadhav <jadhavpratik290@gmail.com>
2022-09-17 12:05:04 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-
-//JSX = Javascript XML
-//Needs to return tag
-//Strict - only one parent tag
-
 function App() {
-  let username = "cdac";
-  const id = "id2";
-  let ref = { id:1, username:"Pratik",
-  id:2, username:"Jadhav",
-  id:3, username:"Satara"}
-                                    
-  let list = ["Pratik", "Patil", "Rahul"];
+  let list = [
 
-  const sayHello = (e) =>{
-    console.log(e);
-  }
-  const sum = () =>{
-    console.log("I am function");
-  }
+    { id: 1, username: "Pratik" },
+    { id: 2, username: "Rahul" },
+  ];
+  
   return (
     <div>
-      <h1>Hello world / Rendered!</h1>
-      {/* Interpolation demo with help of interpollatoion we can write any javascript syntax*/ }
-
-      {/*attribute level interpolation*/}
-      <h1 id={id}>{username}</h1>
-      {/* <h1>{ref.id}</h1>
-      <h1>{ref.username}</h1> */}
-      <h1>{JSON.stringify(ref)}</h1>
-      <h1>{list }</h1>
-      <h1>{sum()}</h1>
-      <input type="button" value="ok" onClick={sayHello}/>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item.id} and {item.username}
+          </li>
+        ))}
+      </ul>
+     
     </div>
   );
 }
-// ES6 syntax
+
 export default App;
