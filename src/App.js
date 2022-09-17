ea2313d199a461e29d602f3cfe5d9338af38db2c
pratik jadhav <jadhavpratik290@gmail.com>
2022-09-17 10:03:36 +0530

    interpolation map and filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+//JSX = Javascript XML
+//Needs to return tag
+//Strict - only one parent tag
 
 function App() {
+  let username = "cdac";
+  const id = "id2";
+  let ref = { id:1, username:"Pratik",
+  id:2, username:"Jadhav",
+  id:3, username:"Satara"}
+                                    
+  let list = ["Pratik", "Patil", "Rahul"];
+
+  const sayHello = (e) =>{
+    console.log(e);
+  }
+  const sum = () =>{
+    console.log("I am function");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world / Rendered!</h1>
+      {/* Interpolation demo with help of interpollatoion we can write any javascript syntax*/ }
+
+      {/*attribute level interpolation*/}
+      <h1 id={id}>{username}</h1>
+      {/* <h1>{ref.id}</h1>
+      <h1>{ref.username}</h1> */}
+      <h1>{JSON.stringify(ref)}</h1>
+      <h1>{list }</h1>
+      <h1>{sum()}</h1>
+      <input type="button" value="ok" onClick={sayHello}/>
     </div>
   );
 }
-
+// ES6 syntax
 export default App;
